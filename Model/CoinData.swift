a1995af280bde45c6cfb50bee1f2dc12f9f9b035
Rinvydas Petniunas <rimvydas.petniunas@gmail.com>
2022-01-26 21:21:07 +0200

    Update CoinData.swift

--- Model/CoinData.swift
+++ Model/CoinData.swift
@@ -8,18 +8,10 @@
 import Foundation
 
 struct CoinData: Decodable {
-   // struct src_side_base: Decodable {
-    //    let asset: String
         let rate: Double
-     //   let SourceBase: [src_side_base]
     }
     
     struct SourceBase: Codable {
         let volume: Double
     }
-
-//    struct Weather: Codable {
-//        let description: String
-//        let id: Int
-//    }
 
